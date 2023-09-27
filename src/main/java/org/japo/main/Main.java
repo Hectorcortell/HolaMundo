38bc2b71c50e6e39101a7e37ae9ecff60ccc3532
Hectorcortell <hectorgradodam@gmail.com>
2023-09-27 18:51:04 +0200

    concatenación de variable: recicled

--- src/main/java/org/japo/main/Main.java
+++ src/main/java/org/japo/main/Main.java
@@ -25,8 +25,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola Mundo");
+    String nombre = "Hectorcortell";
+        System.out.println("Hola " + nombre);
+        
     }
    
    
